5754ba0e093011b62bcd8f46c3e4f1fe2cd022f5
Br66 <br66@njit.edu>
2018-05-20 15:33:44 -0400

    Reorganization of files into tests and exercises, working on in-game grid-based environment editing tool

---
+++ Assets/PlaceholderFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderFunctions : MonoBehaviour {
+
+    int width = 2, height = 2;
+
+    public GameObject DataContext;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // find the mouse
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        // get the nearest grid position
+        mouseX = Mathf.Floor(mouseX / width) * width;
+        mouseY = Mathf.Floor(mouseY / height) * height;
+
+        // change my position to that
+        gameObject.transform.position = new Vector3(mouseX, mouseY);
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        //var overlapping = 
+        Instantiate(DataContext, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Test 2/TenantFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenantFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AttachNewOfficeToMouse()
+    {
+        // attach placeholder to position on grid closest to mouse to show viable placements for new tenant
+    }
+
+    public void AddNewOfficeAtPosition()
+    {
+        // place new tenant at position selected by user via click
+    }
+}
