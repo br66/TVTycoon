8ba360f57a5556d74a61674d46f5f6c0558d609a
Br66 <br66@njit.edu>
2018-06-03 18:17:25 -0400

    Test for object placement validation seems to be working

--- Assets/PlaceholderFunctions.cs
+++ Assets/PlaceholderFunctions.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlaceholderFunctions : MonoBehaviour {
 
-    int width = 2, height = 2;
+    public int width = 2, height = 2;
+
+    public float radius = 1f;
 
     public GameObject DataContext;
 
-	// Use this for initialization
-	void Start () {
+    public List<Collider2D> Colliders;
+
+    public bool canAdd = false;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -26,12 +33,52 @@ public class PlaceholderFunctions : MonoBehaviour {
         // change my position to that
         gameObject.transform.position = new Vector3(mouseX, mouseY);
 
+        //raycast in all directions, find first objects that intersect, see if i'm right next to any of them
+        Colliders = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask("Building")).ToList();
 
+        int i = 0;
+
+        var list = Colliders.Select(x => x.gameObject).ToList();
+
+        for (i = 0; i < list.Count(); i++)
+        {
+            if (name != list[i].name && list.Count < 3)
+            {
+                //Debug.Log(transform.position.x + " || " + (ject.transform.position.x + ject.GetComponent<SpriteRenderer>().bounds.size.x) + " || " + (1 + ject.GetComponent<SpriteRenderer>().bounds.size.x));
+
+                // if on left side
+                if (Mathf.Abs(transform.position.x - (list[i].transform.position.x + list[i].GetComponent<SpriteRenderer>().bounds.size.x)) == (1 + list[i].GetComponent<SpriteRenderer>().bounds.size.x) && Mathf.Abs(transform.position.y - list[i].transform.position.y) == 0)
+                {
+                    Debug.Log("right next to each other, left side");
+                    continue;
+                }
+                // if on right side
+                else if (Mathf.Abs(transform.position.x - (list[i].transform.position.x + list[i].GetComponent<SpriteRenderer>().bounds.size.x)) == 0 && Mathf.Abs(transform.position.y - list[i].transform.position.y) == 0)
+                {
+                    Debug.Log("right next to each other, right side");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if (i != list.Count())
+            canAdd = false;
+        else
+            canAdd = true;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 
     private void OnMouseDown()
     {
-        //var overlapping = 
         Instantiate(DataContext, transform.position, transform.rotation);
     }
 }
